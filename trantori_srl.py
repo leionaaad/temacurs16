5ff49ef0eb3f8517b51292ea6b23dc23274fc725
leionaaad <leionaaad@yahoo.com>
2023-11-12 19:42:30 +0200

    basic structure ready.Nothing works though

---
+++ trantori_srl.py
@@ -0,0 +1,67 @@
+# Creati un repository temacurs16. 
+
+# Scrieti un program de gestiune a angajatilor.
+# Acesta trebuie sa retina pentru fiecare angajat numele prenumele si salariul intr-un fisier (alegeti voi daca txt csv sau json). 
+# Programul trebuie sa permita: 
+# 1.introducerea unui angajat nou (adauga datele angajatului in fisier - datele sunt citite de la tastatura)
+# 2.stergerea unui angajat pe baza numelui. Angajatul este sters din fisier. 
+# 3.afisare toti angajatii. 
+# 4.Pentru ca urilizatorul sa selecteze ce vrea sa faca ii vom crea la final un meniu ( sa aleaga 1 ptistare angajati, 2 pt adaugare etc) 
+
+# Pentru fiecare punct de mai sus( fiecare feature) creati un branch nou (numele branchului sa fie sugestiv referitor la feature-ul pe care 
+#il implementat),  si apoi faceti pull request catre master. 
+
+
+
+import json
+
+
+
+def trantori_management_extraordinaire():
+    action = None
+    options = {"angajare": "A", "concediere": "C", "listare": "L"}
+    while action not in options.values():
+        action = input(f"Ce facem astazi, sefu?\n {options['angajare']} - Angajam, {options['concediere']} - Concediem, {options['listare']} - Ne uitam la lista?").upper()
+        if action == "A":
+            hireTrantor()
+        elif action == "C":
+            fireTrantor()
+        elif action == "L":
+            printAllTrantori()
+        else:
+            print("Sefu, se pare ca nu sunteti atent. Nu-i nimic, incercam inca odata.\n")
+
+
+
+def saveTrantorData(outputPath):
+    pass
+
+
+
+def removeTrantorData(outputPath, entry):
+    pass
+
+
+
+def readTrantorData(inputPath):
+    pass
+
+
+
+def hireTrantor():
+    pass
+
+
+
+def fireTrantor():
+    pass
+
+
+
+def printAllTrantori():
+    pass
+
+
+
+#run the damned thing
+trantori_management_extraordinaire()
